ba349481164aa01be3960764aad8ed7685d17712
Kamile Svambaryte <KSUA1010@G.EDUCAAND.ES>
2025-01-24 11:32:10 +0100

    Add files via upload

---
+++ activdad2/js/script.js
@@ -0,0 +1,11 @@
+function mostrarAlerta() {
+    alert("¡hola cayetano! eres el mas guapo");
+    terceraFuncion();
+}
+function segundaFuncion() {
+ terceraFunciondaFuncion("adios cayetano eres el mejor");
+}
+function terceraFuncion() {
+ alert("<3 ;)");
+}
+    
