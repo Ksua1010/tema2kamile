82fae34604325989ba2f68dee3be414deec63707
Kamile Svambaryte <KSUA1010@G.EDUCAAND.ES>
2025-01-31 11:42:47 +0100

    Add files via upload

---
+++ actividad4/js/script.js
@@ -0,0 +1,7 @@
+function calcular() {
+  let a;
+  let b;
+a=parseInt(prompt("introduce el primer número a"));
+b=parseInt(prompt("introduce el segundo número b"));
+alert("el primer número es "+ (a) + "\nel segundo numero es "+ (b) +"\nla suma es "+ (a+b) +" \nla resta es "+ (a-b)+"\nel producto es "+ (a*b) +"\nla división es "+ (a/b) +" \nla resto es "+ (a%b));
+   }
