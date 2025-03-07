6a7447ba0d94d6a914dfafb345b11fe4734e8d13
Kamile Svambaryte <KSUA1010@G.EDUCAAND.ES>
2025-03-07 11:58:36 +0100

    Add files via upload

---
+++ actividad 8/js/script.js
@@ -0,0 +1,41 @@
+function miau(){
+  let respuesta = parseInt(prompt("resultado"));
+  switch(respuesta) {
+    case 7:
+      case 8:
+        case 9:
+       case 10:
+     case 11:
+            alert("buenos días guapi :)")
+    break;
+      case 12:
+        case 13:
+          case 14:
+            case 15:
+              case 16:
+                case 17:
+              case 18:
+            case 19: 
+          case 20:
+        case 21: 
+            alert("buenas tarde guapi ;) ")
+    break;
+     case 22:
+       case 23:
+         case 24:
+          case 1:
+            case 2:
+         case 3:
+        case 4:
+       case 5:
+     case 6:
+      alert("buenas noches <3")
+    break;
+    default:
+      alert("la hora introducida no es válida")
+    break;
+
+          }
+
+
+}
